f9d3e3431ff6bedfb4a8a6626c59e8ee7be2fafb
dieuaime-shami <shamidieuaime272@gmail.com>
2025-09-01 00:18:28 +0200

    feat/ form of collectiong the skill and career

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import './App.css'
 import { HomeFullContainer } from './components/homePage/FullContainer'
 
 
+
 function App() {
 
   return (
     <>
+    
       <HomeFullContainer />
+
     </>
   )
 }
--- src/components/homePage/FullContainer.jsx
+++ src/components/homePage/FullContainer.jsx
@@ -1,11 +1,13 @@
 import { Navigation } from "./navigation"
 import { InputTest } from "./test"
 import { TopContainer } from "./TopPage"
+import LogForm from "../guidancePage/LogForm"
 
 export const HomeFullContainer = () => {
 
     return <div className="w-screen ">
         <Navigation />
+        <LogForm />
         <TopContainer />
         {/* <InputTest /> */}
     </div>
